f195ffb62cfef0596af47cded69bfec46a141d70
DannyZz <dannyzabaleta0@gmail.com>
2025-11-13 13:35:15 -0500

    Use environment variables for DB credentials
    Replaced hardcoded MySQL credentials in SQLALCHEMY_DATABASE_URI with values loaded from environment variables for improved security and configurability.

--- app/config.py
+++ app/config.py
@@ -4,5 +4,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://root:@localhost/studentVerifier"
+    SQLALCHEMY_DATABASE_URI = f"mysql+mysqldb://{os.getenv("db_user")}:{os.getenv("db_pass")}@localhost/{os.getenv("db_name")}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
